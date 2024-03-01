7058af8ecbe23c340b4e0ade018840c62a8e1034
Boe Kaung Kyaw <148479413+boekaungkyaw22@users.noreply.github.com>
2024-03-01 09:10:55 +0000

    with ony nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <main>
       <NavBar/>
-     <AnimatedBackground/>
+     {/* <AnimatedBackground/>
      <StickyScrollRevealDemo/>
-     <TracingBeamDemo/>
+     <TracingBeamDemo/> */}
     </main>
   );
 }

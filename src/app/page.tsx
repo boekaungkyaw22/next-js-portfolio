18722d2378022676745a257d73f1a0c6c5f128b3
Boe_Kaung_Kyaw <boekaung.kyaw@kbzbank.com>
2024-05-19 22:40:33 +0630

    Floting Navbar Modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
      <AnimatedBackground/>
      {/* <StickyScrollRevealDemo/>
      <TracingBeamDemo/> */}
-     {/* <Footer/> */}
+     {/* <Footer/>   */}
     </main>
   );
 }
